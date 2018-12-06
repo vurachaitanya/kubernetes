a372689b384d8fd8c96d6ac50e4bc053c110fd05
vurachaitanya <vurachaitanya@gmail.com>
2018-12-06 22:14:51 +0530

    updated script

--- k8s_build_ubuntu_script/k8s_install.sh
+++ k8s_build_ubuntu_script/k8s_install.sh
@@ -1,4 +1,11 @@
-sudo apt-get update && apt-get install -y apt-transport-https
+#!/bin/bash
+
+#sudo apt-get update && apt-get install -y apt-transport-https
+sleep 2
+echo "updating repos..."
+
+#if [[ $? == 0 ]]
+#then
 sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
@@ -6,12 +13,16 @@ sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
 sudo cat << EOF > /etc/apt/sources.list.d/kubernetes.list 
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+#fi
+
 apt-get update
+sleep 3
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+sleep 2
 sudo kubeadm init
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+#mkdir -p $HOME/.kube
+#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#sudo chown $(id -u):$(id -g) $HOME/.kube/config
+#sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+#sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
